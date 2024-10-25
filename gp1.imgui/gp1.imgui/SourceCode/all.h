ce1312eeabff826d091f3e195da999ad13272c40
masa433 <masa1107s@icloud.com>
2024-10-25 19:27:11 +0900

    更新

---
+++ gp1.imgui/gp1.imgui/SourceCode/all.h
@@ -0,0 +1,27 @@
+#ifndef ALL_H
+#define ALL_H
+
+//******************************************************************************
+//
+//
+//      all.h
+//
+//
+//******************************************************************************
+
+//------< インクルード >---------------------------------------------------------
+#include "../GameLib/game_lib.h"
+#include "../GameLib/input_manager.h"
+#include "../GameLib/obj2d_data.h"
+
+#include "common.h"
+#include "scene_game.h"
+
+#ifdef USE_IMGUI
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_internal.h"
+#include "../imgui/imgui_impl_dx11.h"
+#include "../imgui/imgui_impl_win32.h"
+#endif
+
+#endif//ALL_H
