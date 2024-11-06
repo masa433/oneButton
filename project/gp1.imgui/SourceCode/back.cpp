9766fdf87d886a323a7b89c426367d50a6683a2c
masa433 <masa1107s@icloud.com>
2024-11-06 09:29:21 +0900

    更新

---
+++ project/gp1.imgui/SourceCode/back.cpp
@@ -0,0 +1,25 @@
+#include"back.h"
+
+int back_state;
+
+using namespace std;
+
+void back_init() 
+{
+
+}
+
+void  back_deinit() 
+{
+
+}
+
+void back_update() 
+{
+
+}
+
+void back_render()
+{
+
+}
