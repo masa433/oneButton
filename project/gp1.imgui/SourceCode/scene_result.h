d55771913be965a2de028825ef543e9f6910a9f5
kw2103 <yuuki.0821.0821.2005@gmail.com>
2024-11-12 15:52:44 +0900

    リザルト（仮）

---
+++ project/gp1.imgui/SourceCode/scene_result.h
@@ -0,0 +1,16 @@
+#ifndef SCENE_RISULT_H
+#define SCENE_RISULT_H
+
+#include "../GameLib/vector.h"
+#include "../GameLib/sprite.h"
+#include "../GameLib/util.h"
+
+void result_init();
+void result_deinit();
+void result_update();
+void result_render();
+
+
+
+
+#endif
