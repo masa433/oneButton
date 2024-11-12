fa005b0d9b4e9b7c8b6b16dd4d042bdfcf10a132
riku22224444 <2240151@ecc.ac.jp>
2024-11-12 16:26:52 +0900

    a

--- project/gp1.imgui/SourceCode/bird.cpp
+++ project/gp1.imgui/SourceCode/bird.cpp
@@ -3,6 +3,24 @@
 #include "common.h"
 
 
+//import time
+//import random
+//
+//def move_bird() :
+//    console_width = 40
+//    bird_symbol = "??"
+//
+//    direction = random.choice(["left", "right"])
+//    if direction == "left" :
+//        for i in range(console_width) :
+//            print(" " * i + bird_symbol, end = "\r", flush = True)
+//            time.sleep(0.1)
+//    else:
+//for i in range(console_width, 0, -1) :
+//    print(" " * (i - 1) + bird_symbol, end = "\r", flush = True)
+//    time.sleep(0.1)
+//
+//    time.sleep(random.uniform(1, 3))  //1•b‚©‚ç3•b‚ÌŠÔŠu
 
 
 
