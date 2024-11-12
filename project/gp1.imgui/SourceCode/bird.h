0ac0bfb87e5152249769ef45a746a6187d53238e
riku22224444 <2240151@ecc.ac.jp>
2024-11-12 16:03:13 +0900

    更新

---
+++ project/gp1.imgui/SourceCode/bird.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef BIRD_H
+#define BIRD_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void game_over();
+void bird_moveX();
+void bird_init();
+void bird_deinit();
+void bird_update();
+void bird_render();
+
+#endif
