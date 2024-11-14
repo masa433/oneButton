413e00f1df9d6cf15be9a824e0d4cbaa07d1e52a
riku22224444 <2240151@ecc.ac.jp>
2024-11-14 18:29:04 +0900

    bird（途中）
    エラー祭りでコメントアウト

--- project/gp1.imgui/SourceCode/bird.cpp
+++ project/gp1.imgui/SourceCode/bird.cpp
@@ -1,49 +1,128 @@
-#include"bird.h"
-#include"system.h"
-#include "common.h"
-
-
-//import time
-//import random
+//#include"bird.h"
+//#include"system.h"
+//#include "common.h"
+//#include    <iostream>
+//#include    <stdlib.h>
+//#include    <time.h>
 //
-//def move_bird() :
-//    console_width = 40
-//    bird_symbol = "??"
+//BIRD   bird[BIRD_MAX];
 //
-//    direction = random.choice(["left", "right"])
-//    if direction == "left" :
-//        for i in range(console_width) :
-//            print(" " * i + bird_symbol, end = "\r", flush = True)
-//            time.sleep(0.1)
-//    else:
-//for i in range(console_width, 0, -1) :
-//    print(" " * (i - 1) + bird_symbol, end = "\r", flush = True)
-//    time.sleep(0.1)
+//class BIRD_DATA {
+//public:
+//    Sprite* spr;
+//    const wchar_t* filePath;
+//    VECTOR2         texPos;
+//    VECTOR2         texSize;
+//    VECTOR2         pivot;
+//    float           radius;
+//}
+//birdData[] = {
 //
-//    time.sleep(random.uniform(1, 3))  //1•b‚©‚ç3•b‚ÌŠÔŠu
-
-
-
-
-
-
-void bird_init() {
-
-}
-void bird_deinit() {
-
-}
-void bird_update() {
-
-}
-void bird_render() {
-
-}
-void bird_moveX() {
-
-}
-//void game_over() {
-//	HP--;
-//	if (HP <= 0) {
-//		nextScene = SCENE_OVER;
-//	}
+//{ NULL, L"./Data/Images/boss.png",     { 0, 0 }, { 80, 80 }, { 40, 40 }, 45},};
+//
+//
+//class BIRD_SET {
+//public:
+//    int             birdType;
+//    VECTOR2         pos;
+//}
+//birdSet[] = {
+//    { 0, { 1000, 600 }},
+//    { 0, { 950, 600 }},
+//    { 0, { 1200, 600 }},
+//    { 0, { 1200, 600 }},
+//    { 0, { 950, 600 }},
+//    { 0, { 700, 600 }},
+//    { 0, { 1200, 600 }},
+//    { 0, { 950, 600 }},
+//    { 0, { 700, 600 }},
+//    { 1, { 1000, 600}},
+//    { 2, { 1200, 600}},
+//    { -1, { -1,  -1 }},
+//};
+//
+//Sprite* sprBird;
+//
+//
+//
+//void bird_init()
+//{
+//    bird[BIRD_MAX].bird_state = 0;
+//  
+//
+//}
+//
+//void bird_deinit()
+//{
+//    int dataNum = ARRAYSIZE(birdData);
+//    for (int i = 0; i < dataNum; ++i) {
+//        safe_delete(birdData[i].spr);
+//    }
+//}
+//
+//void bird_update()
+//{
+//    switch (bird[BIRD_MAX].bird_state)
+//    {
+//    case 0:
+//    {
+//        int dataNum = sizeof(birdData) / sizeof(BIRD_DATA);
+//
+//
+//        for (int i = 0; i < dataNum; ++i) {
+//            birdData[i].spr = sprite_load(birdData[i].filePath);
+//
+//        }
+//    }
+//
+//    ++bird[BIRD_MAX].bird_state;
+//
+//    case 1:
+//
+//        for (int i = 0; i < BIRD_MAX; ++i) {
+//            bird[i] = {};
+//            bird[i].moveAlg = -1;
+//        }
+//
+//        for (int i = 0; birdSet[i].birdType >= 0; i++) {
+//           BIRD* p = searchSet0(bird, BIRD_MAX, birdSet[i].birdType, birdSet[i].pos);
+//            if (!p)break;
+//        
+//        }
+//        ++bird[BIRD_MAX].bird_state;
+//
+//    case 2:
+//     
+//         /*   ++bird[BIRD_MAX].bird.timer;
+//      */
+//
+//        break;
+//
+//    }
+//}
+//void bird_render() {
+//    for (int i = 0; i < BIRD_MAX; ++i)
+//    {
+//        if (bird[i].moveAlg == -1)continue;
+//        sprite_render(
+//            bird[i].spr,
+//            bird[i].position.x, bird[i].position.y,
+//            bird[i].scale.x, bird[i].scale.y,
+//            bird[i].texPos.x, bird[i].texPos.y,
+//            bird[i].texSize.x, bird[i].texSize.y,
+//            bird[i].pivot.x, bird[i].pivot.y,
+//            ToRadian(0),
+//            bird[i].color.x, bird[i].color.y,
+//            bird[i].color.z, bird[i].color.w
+//        );
+//
+//    }
+//}
+//
+//
+//
+////void game_over() {
+////	HP--;
+////	if (HP <= 0) {
+////		nextScene = SCENE_OVER;
+////	}
--- project/gp1.imgui/SourceCode/bird.h
+++ project/gp1.imgui/SourceCode/bird.h
@@ -1,34 +1,65 @@
-#pragma once
-#ifndef BIRD_H
-#define BIRD_H
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#define BIRD_MAX   (500)
-#define ENEMY_SPEED_X_MAX  (20.0f)   
-
-
-
-
-
-
-//void game_over();
-void bird_moveX();
-void bird_init();
-void bird_deinit();
-void bird_update();
-void bird_render();
-
-#endif
+//#pragma once
+//#ifndef BIRD_H
+//#define BIRD_H
+//#include "../GameLib/vector.h"
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//class  BIRD{
+//public:
+//	float angle;
+//	VECTOR2 speed;
+//	VECTOR2 position;
+//	VECTOR2 scale;
+//	VECTOR2 texPos;
+//	VECTOR2 texSize;
+//	VECTOR2 pivot;
+//	VECTOR4 color;
+//	GameLib::Sprite* spr;
+//	int bird_state;
+//	int moveAlg;
+//
+//
+//
+//};
+//
+//
+//#define BIRD_MAX   (200)
+//#define BIRD_SPEED_X_MAX  (20.0f)   
+//#define BIRD_TEX_W        (169.0f)   // プレイヤーの画像1つの幅
+//#define BIRRD_TEX_H        (180.0f)   // プレイヤーの画像1つの高さ
+//
+//
+//
+//
+//
+////void game_over();
+//void bird_moveX();
+//void bird_init();
+//void bird_deinit();
+//void bird_update();
+//void bird_render();
+//
+//BIRD* searchSet0(BIRD arr[], int dataNum, int moveAlg, VECTOR2 pos)
+//{
+//
+//
+//	for (int i = 0; i < dataNum; i++) {
+//		if (arr[i].moveAlg != -1) continue;
+//
+//		arr[i] = {};
+//		arr[i].moveAlg = moveAlg;
+//		arr[i].position = pos;
+//		return &arr[i];
+//	}
+//	return NULL;
+//
+//#endif
